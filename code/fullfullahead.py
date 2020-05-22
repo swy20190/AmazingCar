9e9cafd56e42da7a107c9baf14a7a79599cc7907
swy20190 <K201410109@163.com>
2020-05-22 21:27:04 +0800

    测试文件，测量小车的最大速度

---
+++ code/fullfullahead.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 全速前进
+import RPi.GPIO as GPIO
+import time
+
+PWMA = 18
+AIN1 = 22
+AIN2 = 27
+
+PWMB = 23
+BIN1 = 25
+BIN2 = 24
+
+
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(AIN2, GPIO.OUT)
+    GPIO.setup(AIN1, GPIO.OUT)
+
+    GPIO.setup(BIN1, GPIO.OUT)
+    GPIO.setup(BIN2, GPIO.OUT)
+    GPIO.setup(PWMB, GPIO.OUT)
+
+
+def forward(speed, duration, l_m, r_m):
+    l_m.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, False)
+    GPIO.output(AIN1, True)
+
+    r_m.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, False)
+    GPIO.output(BIN1, True)
+
+    time.sleep(duration)
+
+
+def brake(duration, l_m, r_m):
+    l_m.ChangeDutyCycle(0)
+    GPIO.output(AIN2, False)
+    GPIO.output(AIN1, False)
+
+    r_m.ChangeDutyCycle(0)
+    GPIO.output(BIN2, False)
+    GPIO.output(BIN1, False)
+
+    time.sleep(duration)
+
+
+if __name__ == '__main__':
+    init()
+    L_Motor = GPIO.PWM(PWMA, 100)
+    L_Motor.start(0)
+    R_Motor = GPIO.PWM(PWMB, 100)
+    R_Motor.start(0)
+    # 全速前进100秒
+    forward(100, 100.0, L_Motor, R_Motor)
+    # 停车3秒
+    brake(3.0, L_Motor, R_Motor)
+    GPIO.cleanup()
+    # 此时测量小车的位移
